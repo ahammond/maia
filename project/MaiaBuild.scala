4cad5fcb1edfc85504468a5ed0af988289f03ec9
Nelson Chen <crazysim@gmail.com>
2013-03-17 00:02:12 -0700

    initial commit

---
+++ project/MaiaBuild.scala
@@ -0,0 +1,19 @@
+import sbt._
+import sbt.Keys._
+
+object MaiaBuild extends Build {
+
+  lazy val maia = Project(
+    id = "maia",
+    base = file("."),
+    settings = Project.defaultSettings ++ Seq(
+      name := "Maia",
+      organization := "com.mindflakes",
+      version := "0.1-SNAPSHOT",
+      scalaVersion := "2.10.0",
+      scalacOptions ++= Seq("-feature", "-deprecation"),
+      resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
+      libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.1.2"
+    )
+  )
+}
