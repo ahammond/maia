7f6926cf4d444a092c83a1b463a0403f4ebcdaf7
Nelson Chen <crazysim@gmail.com>
2013-03-17 18:41:32 -0700

    add typesafe config library

--- project/MaiaBuild.scala
+++ project/MaiaBuild.scala
@@ -15,7 +15,8 @@ object MaiaBuild extends Build {
       resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
       libraryDependencies ++= Seq(
         "com.typesafe.akka" %% "akka-actor" % "2.1.2",
-        "pircbot" % "pircbot" % "1.5.0"
+        "pircbot" % "pircbot" % "1.5.0",
+        "com.typesafe" % "config" % "1.0.0"
       )
     )
   )
