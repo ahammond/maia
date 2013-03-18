b6ba9565f35db36ba17b89dacbf73f0c0f39d247
Nelson Chen <crazysim@gmail.com>
2013-03-18 10:09:29 -0700

    delete whitespace

--- src/main/scala/com/mindflakes/Maia.scala
+++ src/main/scala/com/mindflakes/Maia.scala
@@ -4,12 +4,6 @@ import akka.actor._
 import akka.pattern.ask
 import scala.concurrent.duration._
 import akka.util.Timeout
-import org.jibble.pircbot.PircBot
-
-
-
-
-
 
 object Maia extends App {
   val system = ActorSystem("Maia")
