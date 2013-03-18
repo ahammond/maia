ffee0a134b90ff0f18d92c989b317a9879603b0f
Nelson Chen <crazysim@gmail.com>
2013-03-18 12:37:12 -0700

    removed unused imports

--- src/main/scala/com/mindflakes/Maia.scala
+++ src/main/scala/com/mindflakes/Maia.scala
@@ -1,9 +1,6 @@
 package com.mindflakes
 
 import akka.actor._
-import akka.pattern.ask
-import scala.concurrent.duration._
-import akka.util.Timeout
 
 object Maia extends App {
   val system = ActorSystem("Maia")
