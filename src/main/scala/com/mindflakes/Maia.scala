80cafaf0f3394b85015f2fb20c9ac52f75f60c36
Nelson Chen <crazysim@gmail.com>
2013-03-18 01:13:32 -0700

    wrong way to do it

--- src/main/scala/com/mindflakes/Maia.scala
+++ src/main/scala/com/mindflakes/Maia.scala
@@ -6,24 +6,14 @@ import scala.concurrent.duration._
 import akka.util.Timeout
 import org.jibble.pircbot.PircBot
 
-case class Start()
 
 
-class MaiaIRCActor extends PircBot with Actor with ActorLogging {
-  setName("MaiaIRCActor")
-  def receive = {
-    case Start => {
-      log.info("Starting")
-    }
-  }
-}
+
 
 
 object Maia extends App {
   val system = ActorSystem("Maia")
-  val irc_bot = system.actorOf(Props[MaiaIRCActor], "IRCBot")
 
-  irc_bot ! Start
   println("Press 'Return' key to exit.")
   readLine()
   system.shutdown()
