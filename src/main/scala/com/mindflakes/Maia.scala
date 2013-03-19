028692af8a3a7474f2fd1221c0c0b7c5b52701a0
Nelson Chen <crazysim@gmail.com>
2013-03-18 18:15:35 -0700

    logging seems to work

--- src/main/scala/com/mindflakes/Maia.scala
+++ src/main/scala/com/mindflakes/Maia.scala
@@ -2,30 +2,50 @@ package com.mindflakes
 
 import akka.actor._
 import org.pircbotx.PircBotX
+import org.pircbotx.hooks.ListenerAdapter
+import org.pircbotx.hooks.events.MessageEvent
 
 case class Message()
 
 object Maia extends App {
   val system = ActorSystem("Maia")
   val irc_bot = system.actorOf(Props[MaiaIRCActor],"irc")
+  val logger = system.actorOf(Props[MaiaIRCLogger],"logger")
 
   println("Press 'Return' key to exit.")
   readLine()
   system.shutdown()
 }
 
-class MaiaIRCActor extends Actor {
+class MaiaIRCActor extends Actor with ActorLogging {
   val irc_bot = new PircBotX
   irc_bot.setName("MaiaIRCActor")
   irc_bot.connect("irc.rizon.net")
   irc_bot.joinChannel("#gardening")
   irc_bot.sendMessage("#gardening", "Itzza me, an actor")
+  irc_bot.getListenerManager.addListener(new LogAdapter)
 
   override def postStop() {
     irc_bot.shutdown()
   }
 
+  class LogAdapter extends ListenerAdapter {
+    override def onMessage(event: MessageEvent[Nothing]) {
+      context.system.eventStream.publish(event)
+    }
+  }
+
   def receive = {
+    case _ => {}
+  }
+}
+
+class MaiaIRCLogger extends Actor with ActorLogging {
+  context.system.eventStream.subscribe(self, classOf[MessageEvent[PircBotX]])
+  def receive = {
+    case e: MessageEvent[PircBotX] => {
+      log.info(e.getMessage)
+    }
     case _ => {}
   }
 }
