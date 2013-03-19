59583f413b3cad6a08fe887accff645a0a9c867c
Nelson Chen <crazysim@gmail.com>
2013-03-18 17:45:10 -0700

    rough layout for bot

--- src/main/scala/com/mindflakes/Maia.scala
+++ src/main/scala/com/mindflakes/Maia.scala
@@ -1,12 +1,21 @@
 package com.mindflakes
 
 import akka.actor._
+import org.pircbotx.PircBotX
 
 object Maia extends App {
   val system = ActorSystem("Maia")
+  val irc_bot = new MaiaIRCBot(system)
 
   println("Press 'Return' key to exit.")
   readLine()
+  irc_bot.shutdown()
   system.shutdown()
+}
 
+class MaiaIRCBot(system: ActorSystem) extends PircBotX {
+  setName("MaiaIRCBot")
+  connect("irc.rizon.net")
+  joinChannel("#gardening")
+  log("Joined")
 }
